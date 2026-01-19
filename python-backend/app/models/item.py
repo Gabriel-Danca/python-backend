e29f7a6ce524d9632bae3e1027d12e42403949fb
Gabriel Danca <gabriel.danca@endava.com>
2026-01-19 18:04:54 +0200

    Updated item.py

--- python-backend/app/models/item.py
+++ python-backend/app/models/item.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base import Base
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(200), nullable=False)
